36d2c91330762793de54f179cb3636cff7ac6102
Guilherme Rodrigues <guilherme.rodrigues@estudante.ufscar.br>
2022-08-26 15:44:36 -0300

    error correct

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import { app } from './app';
 
-const port = 4003 || process.env.PORT;
+const port = 4003 || process.env.APP_PORT;
+
+app.get("/", (request: Request, response: Response) => {
+    return response.send("🖌️  Server Online!");
+})
 
 app.listen(port, () => console.log('🖌️  Server Online!'));
