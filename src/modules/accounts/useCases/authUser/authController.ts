f1d49755aa161f4d53eedf61f9d54a5b7770ecde
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2022-10-21 18:54:20 -0300

    fix

--- src/modules/accounts/useCases/authUser/authController.ts
+++ src/modules/accounts/useCases/authUser/authController.ts
@@ -5,6 +5,7 @@ import { AuthUseCase } from './authUseCase';
 export class AuthController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
+
     const authUseCase = new AuthUseCase();
 
     const res = await authUseCase.execute({
